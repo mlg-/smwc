a0c57370526953dbd287fa47451574dd7aded389
MLG <melissaleighgore@gmail.com>
2015-01-03 14:32:46 -0500

    Footer, Sitemap, beginnings of blog styling

--- wp-content/themes/smwc_withlove/footer.php
+++ wp-content/themes/smwc_withlove/footer.php
@@ -11,12 +11,24 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
+		
+		<div class="left-footer-column">
+				<div class="aligncenter">
+					<h1 class="mission">The Southern Maine Workers’ Center is a nonprofit membership organization committed to creating a grassroots, people-powered movement that improves the lives, working conditions, and terms of employment for working and poor people in Maine.</h1>
+				<a href="/donate" class="donatebutton">Donate</a>
+			</div>
+
+		</div>
+		<div class="right-footer-column">
 			<?php wp_nav_menu( array( 'theme_location' => 'footer-menu', 'container_class' => 'footer_menu_class' ) );
 			 wp_footer(); ?>
- 		<div class="site-info">
+			<h1 class="funders">With Gratitude to Our Funders</h1>
+			<a href="http://www.iwj.org"><img src="/wp-content/themes/smwc_withlove/images/IWJ.jpg" alt="Interfaith Worker Justice Logo"/></a>
+			<a href="http://resist.org"><img src="/wp-content/themes/smwc_withlove/images/RESIST.png" alt="RESIST Logo"></a>
+		</div>		
+		<div class="site-info">
 			<?php printf( __( '%1$s by %2$s.', 'SMWC' ), 'SMWC', '<a href="http://mlg.codes" rel="designer">MLG &#10085;</a>' ); ?>
 		</div><!-- .site-info -->
-		</div><!-- #page -->
 
  	</footer><!-- #colophon -->
 
--- wp-content/themes/smwc_withlove/index.php
+++ wp-content/themes/smwc_withlove/index.php
@@ -13,34 +13,70 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div id="primary" class="content-area">
 
-		<?php if ( have_posts() ) : ?>
+<div class="background-container">
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+	<div class="page-container">
+					<div class="sidebar">
+			<?php
+			  if($post->post_parent) {
+			  $children = wp_list_pages("title_li=&child_of=".$post->post_parent."&echo=0");
+			  $titlenamer = get_the_title($post->post_parent);
+				$parentlink = get_permalink($post->post_parent);
+							  }
 
-				<?php
-					/* Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_format() );
-				?>
+				  else {
+				  $children = wp_list_pages("title_li=&child_of=".$post->ID."&echo=0");
+				  $titlenamer = get_the_title($post->ID);
+				  }
+				  if ($children) { ?>
 
-			<?php endwhile; ?>
+				  <h2 class="sidebar-title"><a href="<? echo $parentlink; ?>"> <? echo $titlenamer; ?></a> </h2>
+				  <ul class="sidebar-items">
+				  <?php echo $children; ?>
+				  </ul>
 
-			<?php smwc_withlove_paging_nav(); ?>
+				<?php } ?>	
+			</div><!--close sidebar-->
 
-		<?php else : ?>
+		<div class="page-content-area">
 
-			<?php get_template_part( 'content', 'none' ); ?>
+			<main id="main" class="site-main" role="main">
 
-		<?php endif; ?>
+				<?php if ( have_posts() ) : ?>
+
+					<?php /* Start the Loop */ ?>
+					<?php while ( have_posts() ) : the_post(); ?>
+
+						<?php ?>
+							<header class="entry-header">
+							<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+							</header><!-- .entry-header -->
+								<?php the_post_thumbnail('', array('class' => 'featured-image')); ?>
+							<div class="entry">
+							<?php the_content();
+						?>
+					</div>
+						<footer class="entry-footer">
+							<?php edit_post_link( __( 'Edit', 'smwc_withlove' ), '<span class="edit-link">', '</span>' ); ?>
+							<?php echo comments_number( $zero, $one, $more ); ?> 
+						</footer><!-- .entry-footer -->
+
+					<?php endwhile; ?>
+
+					<?php smwc_withlove_paging_nav(); ?>
+
+				<?php else : ?>
+
+					<?php get_template_part( 'content', 'none' ); ?>
+
+				<?php endif; ?>
 
 		</main><!-- #main -->
-	</div><!-- #primary -->
+	</div><!-- page container -->
+</div><!--background container-->
+</div><!--content area-->
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
