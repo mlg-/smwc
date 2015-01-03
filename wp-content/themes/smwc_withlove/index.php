f1045607f36df85e518cb19fb85e770d9704a89e
MLG <melissaleighgore@gmail.com>
2015-01-03 15:09:13 -0500

    Basic and ugly blog styling changes

--- wp-content/themes/smwc_withlove/index.php
+++ wp-content/themes/smwc_withlove/index.php
@@ -13,34 +13,42 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div class="background-container">
 
-		<?php if ( have_posts() ) : ?>
+	<div class="page-container">
+		
+		<div id="primary" class="content-area">
+			<h1 class="aligncenter">BLOG</h1>
+		
+			<main id="main" class="site-main" role="main">
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+				<?php if ( have_posts() ) : ?>
 
-				<?php
-					/* Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_format() );
-				?>
+					<?php /* Start the Loop */ ?>
+					<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php endwhile; ?>
+						<?php
+							/* Include the Post-Format-specific template for the content.
+							 * If you want to override this in a child theme, then include a file
+							 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+							 */
+							get_template_part( 'content', get_post_format() );
+						?>
 
-			<?php smwc_withlove_paging_nav(); ?>
+					<?php endwhile; ?>
 
-		<?php else : ?>
+					<?php smwc_withlove_paging_nav(); ?>
 
-			<?php get_template_part( 'content', 'none' ); ?>
+				<?php else : ?>
 
-		<?php endif; ?>
+					<?php get_template_part( 'content', 'none' ); ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+				<?php endif; ?>
+
+				</main><!-- #main -->
+			</div><!-- #primary -->
+		</div><!--page container-->
+	</div><!--background container-->
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
